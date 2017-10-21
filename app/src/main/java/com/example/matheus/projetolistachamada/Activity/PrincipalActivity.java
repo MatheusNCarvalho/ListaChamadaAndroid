8a90f9ed39c7c1a659b06741ea33eb95feea78a5
MatheusNCarvalho <matheusnevesdecarvalho@gmail.com>
2017-10-20 22:12:40 -0200

    Adicionado Tela principal

---
+++ app/src/main/java/com/example/matheus/projetolistachamada/Activity/PrincipalActivity.java
@@ -0,0 +1,15 @@
+package com.example.matheus.projetolistachamada.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.matheus.projetolistachamada.R;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
