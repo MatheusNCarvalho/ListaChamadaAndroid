f1b1a0b7f209459b0f3705e06b9864427fe1dd76
BrunnoHCInacio <brunnoh11@gmail.com>
2017-11-19 11:38:47 -0200

    add entidade disciplina

---
+++ app/src/main/java/com/example/matheus/projetolistachamada/Entidades/Disciplinas.java
@@ -0,0 +1,29 @@
+package com.example.matheus.projetolistachamada.Entidades;
+
+/**
+ * Created by Brunno on 18/11/2017.
+ */
+
+public class Disciplinas {
+    private String id;
+    private String nome;
+
+    public Disciplinas() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
